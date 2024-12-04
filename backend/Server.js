eefda3f1a09195b6dac79793328f30ff678fd023
Hassan <96440779+hassan6846@users.noreply.github.com>
2024-12-04 18:36:15 +0500

    main

--- backend/Server.js
+++
@@ -1,5 +0,0 @@
-const app = require("./app");
-
-app.listen(process.env.PORT,()=>{
-    console.log(`Server is Running on ${process.env.PORT}`)
-})
--- backend/routes/VideoRoutes.js
+++
@@ -1,8 +0,0 @@
-const express=require("express")
-
-const router=express.Router()
-//controllers
-const Categories = require("../controllers/Videos/GetAllCategories")
-router.route('/videos/categories').get(Categories)
-
-module.exports=router
